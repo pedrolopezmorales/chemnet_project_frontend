27036b1e0211fae4d159924f2f60ba826c56bc6a
pedrolopezmorales <pelomo.mx@gmail.com>
2025-11-06 17:40:15 -0500

    Testing changes for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  images: {
-    unoptimized: true
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
-  // For GitHub Pages deployment
-  basePath: process.env.NODE_ENV === 'production' ? '/chemnet_project_frontend' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/chemnet_project_frontend/' : '',
 };
 
 export default nextConfig;
